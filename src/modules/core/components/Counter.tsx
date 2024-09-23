3b555948274203db8163b8eae481f036bb396ef1
Emmanuel Garnica <edgarnicap95@gmail.com>
2024-09-22 21:30:29 -0500

    config(general): Adding react logo.

--- src/modules/core/components/Counter.tsx
+++ src/modules/core/components/Counter.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import viteLogo from '/vite.svg';
-import reactLogo from '../../assets/react.svg';
+import reactLogo from '../../../assets/react.svg';
 
 const Counter = () => {
   const [count, setCount] = useState(0)
